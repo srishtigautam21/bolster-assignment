a80adf36d90421cd2ebe696f77a36bc2cbd3116a
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2023-08-29 21:06:34 +0530

    Feat : Added images and some css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import ImageComponent from "./component/ImageComponent";
+import { data } from "./data";
 
 function App() {
-  return <div className='App'>hello</div>;
+  data.map((item) =>
+    console.log(item.image2.split("-")[1].includes("correct"))
+  );
+  return (
+    <div className='App'>
+      <div>Timer</div>
+      <ImageComponent data={data} />
+    </div>
+  );
 }
 
 export default App;
